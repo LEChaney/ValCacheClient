054e76bb3ad1b8937789118479389d40b619714d
FurryFur <lance.e.chaney@gmail.com>
2019-08-18 12:05:25 +1200

    Init

--- ValCacheClient/MainWindow.xaml.cs
+++ ValCacheClient/MainWindow.xaml.cs
@@ -11,7 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.ServiceModel;
 
 namespace ValCacheClient
 {
@@ -20,9 +23,145 @@ namespace ValCacheClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<string> RemoteFiles { get; set; }
+        public ObservableCollection<string> LocalFiles { get; set; }
+
+        public const string DOWNLOAD_DIR = @".\downloads";
+
         public MainWindow()
         {
+            this.RemoteFiles = new ObservableCollection<string>();
+            this.LocalFiles = new ObservableCollection<string>();
+
             InitializeComponent();
+
+            // Refresh the list of downloaded files
+            RefreshDownloadedFilesList();
+
+            // Setup folder watcher for displaying downloaded files
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = DOWNLOAD_DIR;
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.*";
+
+            // Add event handlers.
+            watcher.Changed += OnDownloadFolderChanged;
+            watcher.Created += OnDownloadFolderChanged;
+            watcher.Deleted += OnDownloadFolderChanged;
+            watcher.Renamed += OnDownloadFolderChanged;
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnDownloadFolderChanged(object source, FileSystemEventArgs e)
+        {
+            App.Current.Dispatcher.Invoke((Action)delegate
+            {
+                RefreshDownloadedFilesList();
+            });
+        }
+
+        // Refresh locally downloaded files list
+        private void RefreshDownloadedFilesList()
+        {
+            LocalFiles.Clear();
+            DirectoryInfo d = new DirectoryInfo(DOWNLOAD_DIR);
+            FileInfo[] files = d.GetFiles("*.*");
+            foreach (FileInfo file in files)
+            {
+                LocalFiles.Add(file.Name);
+            }
+        }
+
+        // Enable / disable download button based on selection in listview
+        private void RemoteFileList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (RemoteFileList.SelectedItem != null)
+                DownloadBtn.IsEnabled = true;
+            else
+                DownloadBtn.IsEnabled = false;
+        }
+
+        // Download selected file
+        private void Download_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = RemoteFileList.SelectedItem.ToString();
+            string savePath = Path.Combine(DOWNLOAD_DIR, filename);
+
+            using (var client = new FilerServerProxyService.FileServerClient())
+            {
+                try
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+                    using (var fileStream = new FileStream(savePath, FileMode.Create))
+                    {
+                        client.GetFile(filename).CopyTo(fileStream);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceError(ex.Message);
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+        }
+
+        // Refresh the remote file list
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshRemoteFiles();
+        }
+
+        private void RefreshRemoteFiles()
+        {
+            using (var client = new FilerServerProxyService.FileServerClient())
+            {
+                RemoteFiles.Clear();
+
+                try
+                {
+                    string[] availableFiles = client.GetAvailableFiles();
+                    foreach (string file in availableFiles)
+                    {
+                        RemoteFiles.Add(file);
+                    }
+                }
+                // Display error and continue
+                catch (Exception ex)
+                {
+                    Trace.TraceError(ex.Message);
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+        }
+
+        // Opens a downloaded file
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = LocalFilesList.SelectedItem.ToString();
+            string filePath = Path.Combine(DOWNLOAD_DIR, filename);
+
+            try
+            {
+                System.Diagnostics.Process.Start(filePath);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                MessageBox.Show(ex.Message, "Error");
+                RefreshDownloadedFilesList();
+            }
+        }
+
+        private void LocalFilesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LocalFilesList.SelectedItem != null)
+                OpenFileBtn.IsEnabled = true;
+            else
+                OpenFileBtn.IsEnabled = false;
         }
     }
 }
